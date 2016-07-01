ae862af86fa86595339c6c106c61ad20cff4840b
Blake Netzeband <blake.netzeband@gmail.com>
2016-07-01 10:41:52 -0600

    instantiated map

---
+++ client_server/public/javascripts/app.js
@@ -0,0 +1,8 @@
+(function () {
+  'use strict';
+
+  L.mapbox.accessToken = 'pk.eyJ1IjoiYmxha2VmYWNlIiwiYSI6ImNpcTFlenBiZDAweDBmd25vMWJxYTRteGoifQ.VLRmQ5HxMyIdQ6qMF6EJug';
+  const map = L.mapbox.map('map', 'mapbox.streets')
+    .setView([40.0150, -105.2705], 13);
+
+})()
