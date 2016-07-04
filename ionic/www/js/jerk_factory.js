97e4a8f5e8786a6ae7a63e1c0bdad4de7c40382c
Blake Netzeband <blake.netzeband@gmail.com>
2016-07-04 07:41:13 -0600

    adjusting algorithm for http post

--- ionic/www/js/jerk_factory.js
+++ ionic/www/js/jerk_factory.js
@@ -35,21 +35,21 @@
 
       standardizeData: function(recordedData) {
         var routes = [];
-        var route = {};
         var url = "https://jerkmaps.herokuapp.com";
         // var url = 'http://Blake.local:3000';
         var deferred = $q.defer();
         var jerk_value = 0;
+        var counter = 1;
 
 
         for (var i = 0; i < recordedData.length; i++) {
-          var counter = 1;
+          var route = {};
           if (recordedData[i].length === 6) {
             jerk_value = Math.sqrt( Math.pow(recordedData[i][0].x, 2) + Math.pow(recordedData[i][1].y, 2) + Math.pow(recordedData[i][2].z, 2) ) / counter;
             route.jerk_value = jerk_value;
             route.location = [recordedData[i][4].latitude, recordedData[i][5].longitude];
             routes.push(route);
-            routes = {};
+            jerk_value = 0;
             counter = 1;
           } else {
             jerk_value += ( Math.sqrt( Math.pow(recordedData[i][0].x, 2) + Math.pow(recordedData[i][1].y, 2) + Math.pow(recordedData[i][2].z, 2) ) );
