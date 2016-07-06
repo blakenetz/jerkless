01585ad9d0d776273715a5f8ce925a34562650f6
Blake Netzeband <blake.netzeband@gmail.com>
2016-07-06 12:47:43 -0600

    added foursquare data

--- client_server/public/jerk_controller.js
+++ client_server/public/jerk_controller.js
@@ -6,13 +6,52 @@
 
   JerkCtrl.$inject = ['$scope', '$http'];
   function JerkCtrl($scope, $http) {
-
     var routes = [];
     var colors = [];
+    var latlon= [40.018, -105.27];
+    var API_ENDPOINT = 'https://api.foursquare.com/v2/venues/search' +
+    '?client_id=FXYG1VISVFFFBTPQGOHSJG5QMWZ3QCQV2GQVLXO3LOLDKAMN' +
+    '&client_secret=XOH4OBQHFJQEJXIBTI2I4M5UU4TEAV530JPIZYBMYYHQ1GX1' +
+    '&v=20200101' +
+    '&ll=' + latlon +
+    '&query=brewery' +
+    '&m=foursquare';
+    var beer = false;
 
     L.mapbox.accessToken = 'pk.eyJ1IjoiYmxha2VmYWNlIiwiYSI6ImNpcTFlenBiZDAweDBmd25vMWJxYTRteGoifQ.VLRmQ5HxMyIdQ6qMF6EJug';
     var map = L.mapbox.map('map', 'mapbox.streets')
-    .setView([40.018, -105.27], 14);
+    .setView(latlon, 14);
+
+    map.attributionControl
+    .addAttribution('<a href="https://foursquare.com/">Places data from Foursquare</a>');
+
+    var foursquarePlaces = L.layerGroup().addTo(map);
+
+    $scope.beer = function () {
+      if (beer === false) {
+        $http.get(API_ENDPOINT)
+        .then(function (result) {
+          for (var i = 0; i < result.data.response.venues.length; i++) {
+            var venue = result.data.response.venues[i];
+            var latlng = L.latLng(venue.location.lat, venue.location.lng);
+            var marker = L.marker(latlng, {
+              icon: L.mapbox.marker.icon({
+                'marker-color': '#FFDD00',
+                'marker-symbol': 'heart',
+                'marker-size': 'large'
+              })
+            })
+            .bindPopup('<strong><a href="https://foursquare.com/v/' + venue.id + '" target="_blank">' +
+            venue.name + '</a></strong>')
+            .addTo(foursquarePlaces)
+          }
+        })
+        beer = true;
+      } else {
+        map.removeLayer(foursquarePlaces)
+        beer = false;
+      }
+    }
 
     $http.get('/routes')
     .then(function(linepoints) {
