a9ecbc0aa136f2e259c4619b996d40ee0b79c8dc
Blake Netzeband <blake.netzeband@gmail.com>
2016-07-09 12:07:39 -0600

    completed about section

--- client_server/public/jerk_controller.js
+++ client_server/public/jerk_controller.js
@@ -7,8 +7,8 @@
   JerkCtrl.$inject = ['$scope', '$http', 'RouteFactory', 'MarkerFactory'];
   function JerkCtrl($scope, $http, RouteFactory, MarkerFactory) {
     var latlon= [40.018, -105.27];
+    $scope.aboutVis = false;
 
-    var beer = false;
     L.mapbox.accessToken = 'pk.eyJ1IjoiYmxha2VmYWNlIiwiYSI6ImNpcTFlenBiZDAweDBmd25vMWJxYTRteGoifQ.VLRmQ5HxMyIdQ6qMF6EJug';
     var map = L.mapbox.map('map', 'mapbox.light')
     .setView(latlon, 14);
@@ -16,13 +16,15 @@
     var foursquarePlaces = L.layerGroup().addTo(map);
     map.attributionControl
     .addAttribution('<a href="https://foursquare.com/">Places data from Foursquare</a>');
-
     var foursquarePlaces = L.layerGroup().addTo(map);
+    RouteFactory.drawRoutes(map)
 
     $scope.beer = function () {
       MarkerFactory.placeMarkers(latlon, map, foursquarePlaces);
     }
-    
-    RouteFactory.drawRoutes(map)
+
+    $scope.about = function () {
+      $scope.aboutVis = !$scope.aboutVis;
+    }
   }
 })()
